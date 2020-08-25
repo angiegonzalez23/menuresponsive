f9123945331bbaad6eb9090f6de4f329fc81904b
angiegonzalez23 <65702449+angiegonzalez23@users.noreply.github.com>
2020-08-25 09:04:24 -0600

    mejoras en el nav
    se agrego interaccción con js

---
+++ js/js.js
@@ -0,0 +1,11 @@
+var Botonlist4= document.getElementById("listBoton4");
+var Boton4=document.getElementById('boton4')
+
+
+function desplegarBoton4 (){
+	Botonlist4.classList.add('mostrar');
+	
+};
+
+
+boton4.addEventListener("mouseover",desplegarBoton4,true);
